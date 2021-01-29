625f7b72626b57165188a43b34e7b15aef592d67
Joakim Karlsson <joakim.karlsson@futuregames.nu>
2021-01-29 09:52:39 +0100

    init

---
+++ DNA/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+// ### APP DESCRIPTION ###
+// The user input a DNA sequence (A,G,C,T) - only accepts these characters.
+// Translate DNA sequence to Amino Acid - Match a sequence of 3 to corresponding Amino Acid
+// Lookup: Amino Acid Table > ENUM
+// Print out the correct one letter code
+// This below is called reading frame. Only applies if we have a remainder from the DNA sequence.
+// xxx xxx xx = start || x xxx xxx x = middle || xx xxx xxx = end
+// Check the 3s in the sequence and print out from start 3's, middle 3's and from the end 3's.
+
+
+namespace DNA
+{
+    class Program
+    {
+        struct AminoAcid
+        {
+            AminoAcidTable ID;
+            string code;
+        }
+        enum AminoAcidTable
+        {
+            UUU,
+            UUC,
+            UUA,
+            UUG
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
