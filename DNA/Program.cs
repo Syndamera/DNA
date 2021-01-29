d51f170c19846568c9efeb020d99d3749f8d0c28
Joakim Karlsson <joakim.karlsson@futuregames.nu>
2021-01-29 16:37:22 +0100

    clean up

--- DNA/Program.cs
+++ DNA/Program.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 // The user input a DNA sequence (A,G,C,T = U) - only accepts these characters.
 // Translate DNA sequence to Amino Acid - Match a sequence of 3 to corresponding Amino Acid
 // Lookup: Amino Acid Table > ENUM
-// Print out the correct one letter code
+// Print out the correct single letter code
 // This below is called reading frame. Only applies if we have a remainder from the DNA sequence.
 // xxx xxx xx = start || x xxx xxx x = middle || xx xxx xxx = end
 // Check the 3s in the sequence and print out from start 3's, middle 3's and from the end 3's.
