c85dc39c62f50c7c4601ba2677f1b139b4d00372
Joakim Karlsson <joakim.karlsson@futuregames.nu>
2021-01-31 13:03:26 +0100

    reading frames for x^3 and task#1 is completed

--- DNA/Program.cs
+++ DNA/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 // ### APP DESCRIPTION ###
-// TASK #1:
+// TASK #1: DONE
 // The user input a DNA sequence (A,G,C,T = U) - only accepts these characters.
 // Translate DNA sequence to Amino Acid - Match a sequence of 3 to corresponding Amino Acid
 // Lookup: Amino Acid Table > ENUM
@@ -40,17 +40,6 @@ namespace DNA
             List<AminoAcid> acids = new List<AminoAcid>();
             CreateAminoAcids(acids);
 
-            // convert enum into a string
-            //string a = Data.Codon.AAG.ToString();
-            //Console.WriteLine("DEMO DATA STRING: " + a + " ENUM: " + (int)Data.Codon.AAG + "\n");
-
-            // prints out all the info for one amino acid
-            //acids[1].PrintAminoAcidInfo();
-
-            // testing comparing strings
-            //string input = "AUG";
-            //CompareDnaWithAminoAcids(acids, input);
-
             Console.Write("Input DNA code: ");
             string inputConsole = Console.ReadLine().ToUpper();
             inputConsole = string.Concat(inputConsole.Where(c => !char.IsWhiteSpace(c)));
@@ -60,61 +49,93 @@ namespace DNA
             characters = input.ToCharArray();
 
             int remainder = characters.Length % 3;
-            if (remainder != 0)
+            if (remainder != 0 && characters.Length > 2)
             {
-                // TODO: shift chars left and keep all but the remainders.
-                // then shift right and keep all but the remainders.
-                // lastly remove either the first or the last or both first & last chars.
-                if(remainder == 2 || remainder == 1)
+                // Reading frames with different input
+                // 8: (xxx xxx) xx - x (xxx xxx) x   - xx (xxx xxx)
+                // 7: (xxx xxx) x  - x (xxx xxx)     - xx (xxx) xx
+                if (remainder == 2 || remainder == 1)
                 {
-                    // removes the two last characters - start frame
+                    // removes the two last characters - start frame for 7-8 characters
                     string start = string.Empty;
                     for (int i = 0; i < characters.Length - remainder; i++)
                     {
                         char c = characters[i];
                         start += c.ToString();
                     }
-                    Console.WriteLine("START FRAME:" + start);
+                    Console.Write("Sequence 1: ");
                     CompareData(acids, start.ToCharArray());
+                    Console.WriteLine();
 
-                    // removes the first and the last character - middle frame
-                    // 8: (xxx xxx) xx - x (xxx xxx) x   - xx (xxx xxx)
-                    // 7: (xxx xxx) x  - x (xxx xxx)     - xx (xxx) xx
-                    
+                    // removes the first and the last character - middle frame for 7-8 characters
                     string middle = string.Empty;
                     for (int i = 1; i < characters.Length - (remainder - 1); i++)
                     {
                         char c = characters[i];
                         middle += c.ToString();
                     }
-                    Console.WriteLine("MIDDLE FRAME: " + middle);
+                    Console.Write("Sequence 2: ");
                     CompareData(acids, middle.ToCharArray());
+                    Console.WriteLine();
 
-                    // removes the two last characters - end frame
                     int temp = 0;
                     if(remainder == 1)
                     {
                         temp = 2;
                     }
+                    // removes the two first characters and removes the 2 last for 7 characters - end frame
                     string end = string.Empty;
                     for (int i = 2; i < characters.Length - temp; i++)
                     {
                         char c = characters[i];
                         end += c.ToString();
                     }
-                    Console.WriteLine("END FRAME: " + end);
+                    Console.Write("Sequence 3: ");
                     CompareData(acids, end.ToCharArray());
+                    Console.WriteLine();
                 }
             }
             else
             {
                 // TODO: 6: (xxx xxx)    - x (xxx) xx      - xx (xxx) x >> should be 3 different readins from x^3
                 // TODO: Display all the readings as single line, single letter code.
-                CompareData(acids, characters);
+                // first reading frame (xxx xxx)
+                if(characters.Length > 2)
+                {
+                    Console.WriteLine("1nd FRAME:" + input.ToString());
+                    Console.Write("Sequence 1: ");
+                    CompareData(acids, characters);
+                    Console.WriteLine();
+
+                    // second reading frame x (xxx) xx
+                    string second = string.Empty;
+                    for (int i = 1; i < characters.Length - 2; i++)
+                    {
+                        char c = characters[i];
+                        second += c.ToString();
+                    }
+                    Console.WriteLine("2nd FRAME:" + second.ToString());
+                    Console.Write("Sequence 2: ");
+                    CompareData(acids, second.ToCharArray());
+                    Console.WriteLine();
+
+                    // third reading frame xx (xxx) x
+                    string third = string.Empty;
+                    for (int i = 2; i < characters.Length - 1; i++)
+                    {
+                        char c = characters[i];
+                        third += c.ToString();
+                    }
+                    Console.WriteLine("3nd FRAME:" + third.ToString());
+                    Console.Write("Sequence 3: ");
+                    CompareData(acids, third.ToCharArray());
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("ERROR: Your input was only {0:D} character and you need a minimum of 3 characters", characters.Length);
+                }
             }
-            
-            // collect and print data from all the acid objects
-            //PrintAllAminoAcids(acids);
         }
 
         static void CompareData(List<AminoAcid> acids, char[] characters)
@@ -173,14 +194,14 @@ namespace DNA
                     {
                         if (input == codon.ToString())
                         {
-                            Console.WriteLine("FOUND A MATCH: " + input + " " + acid.Name + " " + acid.SingleLetterCode);
+                            //Console.WriteLine("FOUND A MATCH: " + input + " " + acid.Name + " " + acid.SingleLetterCode);
+                            Console.Write(acid.SingleLetterCode);
                             if (acid.StartCodon)
                             {
-                                Console.WriteLine("Start Codon");
+                                //Console.WriteLine("Start Codon");
                             }
-
                             // prints all the info of the matched amino acid
-                            acid.PrintAminoAcidInfo();
+                            //acid.PrintAminoAcidInfo();
                         }
                     }
                 }
